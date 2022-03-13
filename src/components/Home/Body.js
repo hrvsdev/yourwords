5b4aef219ce69bfec709c815f77f39448c59dac8
kingvyas <kumarvyasss@gmail.com>
2022-03-13 09:30:11 +0530

    svn

--- src/components/Home/Body.js
+++ src/components/Home/Body.js
@@ -77,7 +77,42 @@ export default function Body() {
         </div>
         <div className="hdl"></div>
         <div className="hdr">Popular</div>
-        <div className="m-sub-box"></div>
+        <div className="m-sub-box">
+          <div>
+            <img src="https://dummyimage.com/300" />
+            <p className="time">AUG 28, 2022</p>
+            <h4 className="heading">
+              From its medieval origins to the digital era, learn everything
+              there is to know about the ubiquitous lorem ipsum passage
+            </h4>
+            <p className="body">
+              Lorem ipsum, or lipsum as it is sometimes known, is dummy text
+              used in laying out print, graphic or web designs. The passage is
+              attributed to an unknown typesetter in the 15th century who is
+              thought to have scrambled parts of Cicero's De Finibus Bonorum
+            </p>
+            <Link to="/" className="read">
+              Read Article <HiArrowRight />
+            </Link>
+          </div>
+          <div>
+            <img src="https://dummyimage.com/300" />
+            <p className="time">AUG 28, 2022</p>
+            <h4 className="heading">
+              From its medieval origins to the digital era, learn everything
+              there is to know about the ubiquitous lorem ipsum passage
+            </h4>
+            <p className="body">
+              Lorem ipsum, or lipsum as it is sometimes known, is dummy text
+              used in laying out print, graphic or web designs. The passage is
+              attributed to an unknown typesetter in the 15th century who is
+              thought to have scrambled parts of Cicero's De Finibus Bonorum
+            </p>
+            <Link to="/" className="read">
+              Read Article <HiArrowRight />
+            </Link>
+          </div>
+        </div>
         <div className="sub-box-2">
           <div>
             <img src="https://dummyimage.com/300" />
