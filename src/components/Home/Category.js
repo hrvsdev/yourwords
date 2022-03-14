c4038fa807c88297f4b609bbabd64e4ddf6a1215
kingvyas <kumarvyasss@gmail.com>
2022-03-14 09:47:32 +0530

    sxt

--- src/components/Home/Category.js
+++ src/components/Home/Category.js
@@ -1,5 +1,6 @@
 import { NavLink } from "react-router-dom";
 import { FiSearch } from "react-icons/fi";
+import { useRef } from "react";
 
 function Category(props) {
   return (
@@ -14,21 +15,19 @@ function Category(props) {
 
 export default function Home() {
   return (
-    <div>
-      <div className="top-bar">
-        <div className="category">
-          <Category to="/" title="All" />
-          <Category to="featured" title="Featured" />
-          <Category to="popular" title="Popular" />
-          <Category to="world" title="World" />
-          <Category to="business" title="Business" />
-          <Category to="technology" title="Technology" />
-          <Category to="safety" title="Safety" />
-        </div>
-        <div className="search">
-          <input type="text" placeholder="Search" />
-          <FiSearch />
-        </div>
+    <div className="top-bar">
+      <div className="category">
+        <Category to="/" title="All" />
+        <Category to="featured" title="Featured" />
+        <Category to="popular" title="Popular" />
+        <Category to="world" title="World" />
+        <Category to="business" title="Business" />
+        <Category to="technology" title="Technology" />
+        <Category to="safety" title="Safety" />
+      </div>
+      <div className="search">
+        <input type="text" placeholder="Search" />
+        <FiSearch />
       </div>
     </div>
   );
