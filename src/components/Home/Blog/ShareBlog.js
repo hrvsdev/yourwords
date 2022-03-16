55e28fe3f1b95c3dbde400b6f0a4bf16b325c9f6
kingvyas <kumarvyasss@gmail.com>
2022-03-16 13:24:29 +0530

    twfr

--- src/components/Home/Blog/ShareBlog.js
+++ src/components/Home/Blog/ShareBlog.js
@@ -1,27 +1,47 @@
 export default function ShareBlog({ heading }) {
   return (
     <div className="share-blog">
-      <a href="/">
-        <img src="https://i.ibb.co/ydsq47w/002-facebook.png" />
+      <a
+        rel="noopener noreferrer"
+        target="_blank"
+        href={`https://www.facebook.com/sharer.php?u=${window.location.href}`}
+      >
+        <img src="https://i.ibb.co/ydsq47w/002-facebook.png" title="Facebook" />
       </a>
       <a
         rel="noopener noreferrer"
-        href={`mailto:?subject=${heading}&body=${heading}%0D%0A${window.location.href}`}
         target="_blank"
+        href={`mailto:?subject=${heading}&body=${heading}%0D%0A${window.location.href}`}
       >
-        <img src="https://i.ibb.co/gzpWtr9/007-arroba.png" />
+        <img src="https://i.ibb.co/gzpWtr9/007-arroba.png" title="Email" />
       </a>
-      <a target="_blank" href="https://www.google.com">
-        <img src="https://i.ibb.co/bBVd5RG/003-twitter.png" />
+      <a
+        rel="noopener noreferrer"
+        target="_blank"
+        href={`https://twitter.com/intent/tweet?text=${heading}%0D%0A${window.location.href}`}
+      >
+        <img src="https://i.ibb.co/bBVd5RG/003-twitter.png" title="Twitter" />
       </a>
-      <a>
-        <img src="https://i.ibb.co/Rv9mjDH/004-whatsapp.png" />
+      <a
+        rel="noopener noreferrer"
+        target="_blank"
+        href={`https://api.whatsapp.com/send?text=${heading}%0D%0A${window.location.href}`}
+      >
+        <img src="https://i.ibb.co/Rv9mjDH/004-whatsapp.png" title="Whatsapp" />
       </a>
-      <a href="/">
-        <img src="https://i.ibb.co/qCYW8Tx/005-reddit.png" />
+      <a
+        rel="noopener noreferrer"
+        target="_blank"
+        href={`https://reddit.com/submit?url=${window.location.href}&title=${heading}`}
+      >
+        <img src="https://i.ibb.co/qCYW8Tx/005-reddit.png" title="Reddit" />
       </a>
-      <a href="/">
-        <img src="https://i.ibb.co/DfmNPPD/006-telegram.png" />
+      <a
+        rel="noopener noreferrer"
+        target="_blank"
+        href={`https://t.me/share/url?url=${window.location.href}&text=${heading}`}
+      >
+        <img src="https://i.ibb.co/DfmNPPD/006-telegram.png" title="Telegram" />
       </a>
     </div>
   );
